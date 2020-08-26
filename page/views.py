0eba4768ec696a67d455b40956cb86b5a4dc394c
Kimtaeho <ben108@naver.com>
2020-08-26 18:29:54 +0900

    ADD CRUD and requirements.txt

--- page/views.py
+++ page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Designer
 
 # Create your views here.
@@ -14,3 +14,44 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk = designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+        #detail은 url path이름
+        #return render('profile/'+str(post.id)) -> 이렇게 url로 쓰기도 함
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+    post.delete()
+
+    return redirect('home')
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+    else:
+        return render(request, 'update.html', {'designer' : post})
